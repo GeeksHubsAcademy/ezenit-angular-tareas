0b454a988944cdaa86306b0b7513f5d1db42e75f
Jupegarnica <juan@geekshubs.com>
2024-08-07 10:09:30 +0200

    chore: Add GetFocusDirective and update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { GetFocusDirective } from "./get-focus.directive";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, GetFocusDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ezenit-angular-tareas';
+  secondsNow = Math.floor(Date.now() / 1000);
+  count = 0;
+
+  smiles = ['😋', '🤑', '🤡']
+
+  inc() {
+    this.count++;
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.secondsNow = Math.floor(Date.now() / 1000);
+    }, 1000);
+  }
+  isHidden() {
+    console.log('isHidden');
+
+   return this.secondsNow % 10 === 0;
+  }
 }
