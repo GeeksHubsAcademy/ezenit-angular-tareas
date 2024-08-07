77fa3c4cd3f5ca8120437569caa22e01e1a0a847
Jupegarnica <juan@geekshubs.com>
2024-08-07 11:28:49 +0200

    refactor: remove unused components and directives from the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { GetFocusDirective } from "./directives/get-focus.directive";
-import { BackgroundDirective } from "./directives/background.directive";
-import {  CounterComponent} from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, GetFocusDirective, BackgroundDirective, CounterComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'ezenit-angular-tareas';
-  secondsNow = Math.floor(Date.now() / 1000);
-  count =  0;
-  colors = ['red', 'green', 'blue'];
-  color= 'red';
-  smiles = ['😋', '🤑', '🤡']
+export class AppComponent {
 
-  inc() {
-    this.count++;
-  }
-
-  ngOnInit() {
-    setInterval(() => {
-      this.secondsNow = Math.floor(Date.now() / 1000);
-      this.color = this.colors[this.secondsNow % 3];
-    }, 1000);
-  }
-  isHidden() {
-    console.log('isHidden');
-
-   return this.secondsNow % 10 === 0;
-  }
 }
--- src/app/directives/get-focus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetFocusDirective } from './get-focus.directive';
-
-describe('GetFocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GetFocusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
