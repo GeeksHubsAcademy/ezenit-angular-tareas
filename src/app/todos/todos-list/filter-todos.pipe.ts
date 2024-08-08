68dbf36c6921b14b0443768aa41c1440a8827aaa
Jupegarnica <juan@geekshubs.com>
2024-08-08 12:10:20 +0200

    feat(todos): add filter functionality to todos list with new pipe and component updates

---
+++ src/app/todos/todos-list/filter-todos.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from '../todos.types';
+import { Filter } from './filter.types';
+
+@Pipe({
+  name: 'filterTodos',
+  standalone: true
+})
+export class FilterTodosPipe implements PipeTransform {
+
+  transform(todos: Todo[], filter: Filter): Todo[] {
+
+    if (filter === 'completed') {
+      return todos.filter(todo => todo.completed);
+    }
+    if (filter === 'uncompleted') {
+      return todos.filter(todo => !todo.completed);
+    }
+    return todos;
+
+  }
+
+}
--- src/app/todos/todos-list/todos-list.component.ts
+++ src/app/todos/todos-list/todos-list.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../todos.types';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { Filter } from './filter.types';
+import { FilterTodosPipe } from './filter-todos.pipe';
+
 
 @Component({
   selector: 'app-todos-list',
   standalone: true,
-  imports: [CommonModule, TodoItemComponent],
+  imports: [CommonModule, TodoItemComponent, FilterTodosPipe],
   templateUrl: './todos-list.component.html',
   styleUrl: './todos-list.component.css'
 })
@@ -14,12 +17,17 @@ export class TodosListComponent {
   @Input() todosList: Todo[] = [];
   @Output() complete = new EventEmitter<Todo>();
   @Output() delete = new EventEmitter<number>();
-
   handleComplete(todo: Todo) {
     this.complete.emit(todo);
   }
   handleDelete(id: number) {
     this.delete.emit(id);
+  }
+
+  currentTab: Filter = 'all';
+  handleTabChange(filter: Filter) {
+    console.log('filter', filter);
+    this.currentTab = filter;
   }
 
 }
